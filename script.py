eed13f9c133a8fd90586403af21665ab88264c8d
zflyluo <zflyluo@tencent.com>
2025-02-17 18:57:43 +0800

    调用AI

--- script.py
+++ script.py
@@ -18,23 +18,23 @@ comment_body = event_data.get('comment', {}).get('body')
 
 
 # Non-streaming:
-print("----- standard event_data:",event_data)
-print("----- standard comment_body:",comment_body)
-# print("----- standard request:",comment_body)
-# gets API Key from environment variable OPENAI_API_KEY
-# client = OpenAI(
-#     api_key=os.getenv('HUNYUAN_API_KEY'), # 混元 APIKey
-#     base_url="https://api.hunyuan.cloud.tencent.com/v1", # 混元 endpoint
-# )
-#
-# completion = client.chat.completions.create(
-#     model="hunyuan-turbo",
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": """请帮我把以下中文文档翻译成英文,并以markdown格式输出:
-# {comment_body}""",
-#         },
-#     ],
-# )
-# print(completion.choices[0].message.content)
+# print("----- standard event_data:",event_data)
+# print("----- standard comment_body:",comment_body)
+print("----- standard request, comment_body:",comment_body)
+gets API Key from environment variable OPENAI_API_KEY
+client = OpenAI(
+    api_key=os.getenv('HUNYUAN_API_KEY'), # 混元 APIKey
+    base_url="https://api.hunyuan.cloud.tencent.com/v1", # 混元 endpoint
+)
+
+completion = client.chat.completions.create(
+    model="hunyuan-turbo",
+    messages=[
+        {
+            "role": "user",
+            "content": """请帮我把以下中文文档翻译成英文,并以markdown格式输出:
+{comment_body}""",
+        },
+    ],
+)
+print(completion.choices[0].message.content)
